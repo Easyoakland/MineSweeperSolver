fca53a4052a7fb361b0f20d5213ce682502c3ecb
EasyOakLand <97992568+Easyoakland@users.noreply.github.com>
2022-03-14 17:09:51 -0600

    implemented class and identify function

--- Main.py
+++ Main.py
@@ -7,17 +7,12 @@ import init
 
 # TODO change layout function to three separate functions for the 3 actions
 
+game = Game()
 
 # Define the possible cells there are
-possibleCells = ["1.png", "2.png", "3.png", "4.png", "flag.png", "complete.png"]
+game.possibleCells = ["1.png", "2.png", "3.png",
+                      "4.png", "flag.png", "cell.png", "complete.png"]
 
-# determine layout of board after confirming it was found
-if determineLayout() == None:
-    print("Couldn't find game")
-    quit()
-else:
-    width, height, cellwidth, cellheight, origin, end, grid = determineLayout()
 
-click((1, 1), grid, width)
-# testing identify function
-# print(identifyCell((1,1),grid,width,cellwidth,cellheight,possibleCells)
+game.click((1, 1))
+print(game.identifyCell((1,1), game.possibleCells))
--- functions.py
+++ functions.py
@@ -10,57 +10,73 @@ import pyautogui
 # pyautogui.click(grid[(2*width)-1])
 
 
-def determineLayout():
-    # set cell width and cell height using first identified cell's width and height
-    # [2:4] is because the function outputs x,y,width,height and we only want width and height
-    temp = pyautogui.locateOnScreen("cell.png")
-    if temp != None:  # if temp is not none
-        cellwidth, cellheight = temp[2:4]
-    else:  # if temp is none
-        return
-
-    # get all cell locations and save to grid var
-    grid = []
-    for cell in pyautogui.locateAllOnScreen("cell.png"):
-        grid.append(cell)
-
-    # Find top left of map and set cords to origin
-    origin = grid[0][0:2]
-    # Find bottom right and set to end
-    end = grid[-1][0:2]
-
-    # set the width and height of the game in cells based upon the width of each cell and the size of the board
-    width = ((end[0]+cellwidth)-origin[0])/cellwidth
-    height = ((end[1]+cellheight)-origin[1])/cellheight
-
-    return width, height, cellwidth, cellheight, origin, end, grid
-
-
-# iterates through all possible images to see if one matches
-# if the image matches the if statement will be true and the image's string will be returned
-def identifyCell(cord, grid, width, cellwidth, cellheight, possibleCells):
-    pos = convertCordToPos(cord, grid, width)
-    for possibleCell in possibleCells:
-        if pyautogui.locateOnScreen(possibleCell, region=(pos[0], pos[1], pos[0]+cellwidth, pos[1]+cellheight)):
-            return possibleCell
+class Game:
+    def __init__(self):
+        # find properties of board after making sure board was found
+        temp = self.determineLayout()
+        if temp == None:  # if board wasn't found temp will have been set to None
+            print("Couldn't find game")
+            quit()
         else:
-            return None
+            self._width, self._height, self._cellwidth, self._cellheight, self._origin, self._end, self._grid = temp
 
+    def determineLayout(self):
+        # set cell width and cell height using first identified cell's width and height
+        # [2:4] is because the function outputs x,y,width,height and we only want width and height
+        temp = pyautogui.locateOnScreen("cell.png")
+        if temp != None:  # if temp is not none
+            cellwidth, cellheight = temp[2:4]
+        else:  # if temp is none
+            return
 
-def convertCordToPos(cord, grid, width):
-    # offsets needed to get to each part of the cord in the grid list
-    # this defines new cords have top left be 0, 0
-    xOffset = cord[0]
-    yOffset = width*cord[1]
-    totalOffset = int(xOffset+yOffset)  # array indice must be of type int
-    return grid[totalOffset]
+        # get all cell locations and save to grid var
+        grid = []
+        for cell in pyautogui.locateAllOnScreen("cell.png"):
+            grid.append(cell)
 
+        # Find top left of map and set cords to origin
+        origin = grid[0][0:2]
+        # Find bottom right and set to end
+        end = grid[-1][0:2]
 
-# clicks at cell cordinate given
-def click(cord, grid, width):
-    pyautogui.click(convertCordToPos(cord, grid, width))
+        # set the width and height of the game in cells based upon the width of each cell and the size of the board
+        width = ((end[0]+cellwidth)-origin[0])/cellwidth
+        height = ((end[1]+cellheight)-origin[1])/cellheight
 
+        return width, height, cellwidth, cellheight, origin, end, grid
 
-# overloaded function clicks on pos given
-def clickPos(pos):
-    pyautogui.click(pos)
+    # iterates through all possible images to see if one matches
+    # if the image matches the if statement will be true and the image's string will be returned
+    def identifyCell(self, cord, possibleCells):
+        pos = self.convertCordToPos(cord)
+        for possibleCell in possibleCells:
+            if pyautogui.locateOnScreen(possibleCell, region=(pos[0], pos[1], self._cellwidth, self._cellheight))!=None:
+                return possibleCell
+        return None
+
+    # convert grid cordinate to pixel position
+    def convertCordToPos(self, cord):
+        # offsets needed to get to each part of the cord in the grid list
+        # this defines new cords have top left be 0, 0
+        xOffset = cord[0]
+        yOffset = self._width*cord[1]
+        totalOffset = int(xOffset+yOffset)  # array indice must be of type int
+        return self._grid[totalOffset][0:2]
+
+    # clicks at cell cordinate given
+    def click(self, cord):
+        pyautogui.click(self.convertCordToPos(cord))
+
+    # click on pos given
+    def clickPos(self, pos):
+        pyautogui.click(pos)
+
+    # possibleCells setter
+    def setPossibleCells(self, possibleCells):
+        self._possibleCells = possibleCells
+
+    # possibleCells getter
+    def getPossibleCells(self):
+        return self._possibleCells
+
+    possibleCells = property(getPossibleCells, setPossibleCells)
