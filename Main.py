34376be62346deab4c5f29932b233f845277650a
EasyOakLand <97992568+Easyoakland@users.noreply.github.com>
2022-03-14 22:28:23 -0600

    Identification works but takes too long

--- Main.py
+++ Main.py
@@ -2,17 +2,32 @@ from functions import *
 import pyautogui
 import init
 
-# TODO Refactor code to use a game class
-# so all these functions don't need to pass every variable every time
+# TODO Change identification function to use pixel color since identifyCell currently takes way too long
 
-# TODO change layout function to three separate functions for the 3 actions
+# TODO logic
+# 1. If the number of cells is equal to the number on the tile then they are all bombs
+# 2. If the number of bombs around a cell is equal to the cell the rest are not bombs
+# 3. Implement linked cells
 
-game = Game()
+
+# TODO give cell a number of mines property
+# iterate through grid creating a cell class instance for each
+
+# remove artificial pausing. use ctrl-alt-delete if needed to abort program
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
+
+game = Game() # setup game class instance
 
 # Define the possible cells there are
 game.possibleCells = ["1.png", "2.png", "3.png",
                       "4.png", "flag.png", "cell.png", "complete.png"]
 
+# make list of IDs for all cells
+cellArray = [game.identifyCellAtPos(pos) for i, pos in enumerate(game._grid)]
 
-game.click((1, 1))
-print(game.identifyCell((1,1), game.possibleCells))
+print(cellArray)
+
+
+cell1 = Cell((0, 0), game)
+game.clickR((2, 2))
--- functions.py
+++ functions.py
@@ -10,6 +10,7 @@ import pyautogui
 # pyautogui.click(grid[(2*width)-1])
 
 
+# Handles info about board itself
 class Game:
     def __init__(self):
         # find properties of board after making sure board was found
@@ -43,16 +44,24 @@ class Game:
         width = ((end[0]+cellwidth)-origin[0])/cellwidth
         height = ((end[1]+cellheight)-origin[1])/cellheight
 
-        return width, height, cellwidth, cellheight, origin, end, grid
+        return int(width), int(height), int(cellwidth), int(cellheight), origin, end, grid
 
     # iterates through all possible images to see if one matches
     # if the image matches the if statement will be true and the image's string will be returned
-    def identifyCell(self, cord, possibleCells):
+    def identifyCell(self, cord):
         pos = self.convertCordToPos(cord)
-        for possibleCell in possibleCells:
-            if pyautogui.locateOnScreen(possibleCell, region=(pos[0], pos[1], self._cellwidth, self._cellheight))!=None:
+        for possibleCell in self.possibleCells:
+            if pyautogui.locateOnScreen(possibleCell, region=(pos[0], pos[1], self._cellwidth, self._cellheight)) != None:
                 return possibleCell
         return None
+    
+    # same as identify cell but takes pixel location
+    def identifyCellAtPos(self, pos):
+        for possibleCell in self.possibleCells:
+            if pyautogui.locateOnScreen(possibleCell, region=(pos[0], pos[1], self._cellwidth, self._cellheight)) != None:
+                return possibleCell
+        return None
+
 
     # convert grid cordinate to pixel position
     def convertCordToPos(self, cord):
@@ -62,10 +71,20 @@ class Game:
         yOffset = self._width*cord[1]
         totalOffset = int(xOffset+yOffset)  # array indice must be of type int
         return self._grid[totalOffset][0:2]
+    
+    # convert pixel position to grid cordinate
+    def convertPosToCord(self, pos):
+        yCord = (pos[1]-self._origin[1])/self._cellheight
+        xCord = (pos[0]-self._origin[0])/self._cellwidth
+        return (int(xCord), int(yCord))
 
     # clicks at cell cordinate given
     def click(self, cord):
         pyautogui.click(self.convertCordToPos(cord))
+
+    # right clicks at cell cordinate given
+    def clickR(self, cord):
+        pyautogui.click(self.convertCordToPos(cord), button="right")
 
     # click on pos given
     def clickPos(self, pos):
@@ -79,4 +98,55 @@ class Game:
     def getPossibleCells(self):
         return self._possibleCells
 
+    # width getter
+    def getWidth(self):
+        return self._width
+
+    # height getter
+    def getHeight(self):
+        return self._height
+
     possibleCells = property(getPossibleCells, setPossibleCells)
+
+
+# Handles an individual cell on the board
+class Cell:
+    def __init__(self, cord, Game):
+        self._ID = Game.identifyCell(cord)
+        self.cord = cord
+        # self._width, self._height = Game.getWidth(), Game.getHeight()
+
+    # returns cords of all neighbors that exist
+    def neighbors(self, radius, game):
+        neighbors = []
+        # goes from left to right and from top to bottom generating neighbor cords
+        # each radius increases number of cells in each dimension by 2 starting with 1 cell at radius = 0
+        for j in range(2*radius+1):
+            for i in range(2*radius+1):
+                neighbors.append(
+                    (self.cord[0]-radius+i, self.cord[1]-radius+j))
+        # removes self from neighbors list
+        neighbors.remove(self.cord)
+        # removes invalid neighbors
+        i = 0
+        while(i < len(neighbors)):
+            cord = neighbors[i]
+            # neighbor has negative cordinate
+            if cord[0] < 0 or cord[1] < 0:
+                neighbors.remove(cord)
+                i -= 1  # move back since index of list will have shifted back
+            # neighbor has cordinate larger than board
+            elif cord[0] > game._width or cord[1] > game._height:
+                neighbors.remove(cord)
+                i -= 1  # move back since index of list will have shifted back
+            i += 1
+        # returns neighbors list
+        return neighbors
+
+    # set ID
+    def setID(self, game):
+        self._ID = Game.identifyCell(self.cord)
+
+    # get ID
+    def getID(self):
+        return self._ID
